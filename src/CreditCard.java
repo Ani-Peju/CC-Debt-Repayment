1e6350d3e56a64ff897f5bb6554d49aa207a49ad
Ani-Peju <pjanimas099@gmail.com>
2026-04-17 18:58:30 -0400

    Added CreditCard class and Calculator class to make project more object-oriented

---
+++ src/CreditCard.java
@@ -0,0 +1,35 @@
+public class CreditCard {
+    private final double  limit;
+    private final double balance;
+    private final double APR;
+
+    public CreditCard(double limit, double balance, double APR) {
+        this.limit = limit;
+        this.balance = balance;
+        this.APR = APR;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public double getUtilization() {
+        return (balance / limit) * 100;
+    }
+
+    public String getCreditScoreCategory() {
+        double utilization = getUtilization();
+
+        if (utilization >= 80) return "Poor";
+        else if (utilization >= 50) return "Fair";
+        else if (utilization >= 30) return "Good";
+        else return "Excellent";
+
+    }
+
+    public double getMonthlyRate() {
+        return (APR / 100) / 12;
+    }
+
+
+}
